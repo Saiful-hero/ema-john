dc7c8d62020d8dbefec242c0e3099e054f9082fe
Saiful-hero <saif061096@gmail.com>
2022-03-07 16:15:49 +0600

    The project has done almost successfully

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Cart = (props) => {
+    console.log(props.cart);
+    console.log(props);
+    const cart = props.cart;
+    // const total = cart.reduce((total,prd)=>total+prd.price,0);
+    let total = 0;
+    for(let i=0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+
+    let shipping = 0;
+    if(total > 35){
+        shipping = 0;
+    }
+    else if(total >15){
+        shipping=4.99;
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+
+    const tax = (total *.1).toFixed(2);
+    const grandTotal = (total+shipping+Number(tax)).toFixed(2);
+    return (
+        <div>
+            <h1>Order Summery</h1>
+            <h4>Items Ordered:{cart.length}</h4>
+            <p>Product Price:{total}</p>
+            <p><small>Shipping Cost:{shipping}</small></p>
+            <p><small>Tax:{tax}</small></p>
+            <p>Total:{grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
